55ec0b9eb7cef67c049e57d11a20109b7e2baa0a
Danik_VItek <x3665107@gmail.com>
2021-09-05 18:17:27 +0300

    initial commit

---
+++ Catmull-Rom Splines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catmull_Rom_Splines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
